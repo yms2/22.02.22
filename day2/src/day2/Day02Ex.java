911689ca4a5dc143da6aba05deee1842bc8f30f6
yms2 <90673332+yms2@users.noreply.github.com>
2022-02-22 17:20:17 +0900

    Add files via upload

---
+++ day2/src/day2/Day02Ex.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Day02Ex {
+
+	public static void main(String[] args) {
+//		System.out.println("문자열값");//문자열값
+//		System.out.println('A');//문자형값
+//		System.out.println(100);//정수형값
+//		System.out.println(3.14);//실수형값
+		//sysout 커서 뒤에 Ctrl + Space 누르면 자동완성
+		//fomat:형식지정자->%알파벳
+		//System.out.printf("형식지정자",출력값")
+		System.out.printf("%s","문자열값");//문자열:string
+		System.out.printf("%c",65);//문자형: Character
+		System.out.printf("%d",100);//정수값:  Decimal
+		System.out.printf("%f",3.14);//실수값: Float
+	}
+
+}
---
+++ day2/src/day2/day02input.java
@@ -0,0 +1,19 @@
+package day2;
+//1.입력기능을 내코드에 연결시켜줘
+import java.util.Scanner;
+public class day02input {
+
+	public static void main(String[] args) {
+		//2.준비물 :scanner을 sc라고 이름을 사용
+		Scanner sc = new Scanner(System.in);
+		//문자열로 입력받아라 -> 기능자체가 키보드로 입력한 입력값
+//		System.out.println(sc.next());
+		//sc.nextint():정수값으로 입력받아라
+//		System.out.println(sc.nextInt());
+		//sc:nextfloat(): 실수형값으로 입력받아라
+//		System.out.println(sc.nextFloat());
+		System.out.println(sc.nextLine());
+		//띄워쓰기한거까지 출력 가능
+ 	}
+
+}
